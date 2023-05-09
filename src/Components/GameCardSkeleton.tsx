f1123ea5e167f157be2b83a07a94625c1f049e5f
Gleb Kraitcer <kraitcer@gmail.com>
2023-05-09 12:13:26 +0400

    fix bug

--- src/Components/GameCardSkeleton.tsx
+++ src/Components/GameCardSkeleton.tsx
@@ -3,11 +3,10 @@ import { Card, CardBody, Skeleton, SkeletonText } from "@chakra-ui/react";
 const GameCardSkeleton = () => {
   return (
     <Card width="300px" borderRadius={10} overflow="hidden">
-      <Skeleton height="200px">
-        <CardBody>
-          <SkeletonText />
-        </CardBody>
-      </Skeleton>
+      <Skeleton height="200px" />
+      <CardBody>
+        <SkeletonText />
+      </CardBody>
     </Card>
   );
 };
