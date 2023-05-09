1f586f8664f35e1062a00f3e3f0891505a1c0cfd
Gleb Kraitcer <kraitcer@gmail.com>
2023-05-09 16:53:28 +0400

    gener list added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
 import NavBar from "./Components/NavBar";
 import GameGrid from "./Components/GameGrid";
+import GenreList from "./Components/GenreList";
 
 function App() {
   return (
@@ -14,8 +15,8 @@ function App() {
         <NavBar />
       </GridItem>
       <Show above="lg">
-        <GridItem area="aside" bg="gold">
-          aside
+        <GridItem area="aside">
+          <GenreList />
         </GridItem>
       </Show>
       <GridItem area="main">
--- src/Components/GameCardContainer.tsx
+++ src/Components/GameCardContainer.tsx
@@ -1,8 +1,11 @@
-import { Box, SimpleGrid, Text } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
+
 import { ReactNode } from "react";
+
 interface Props {
   children: ReactNode;
 }
+
 const GameCardContainer = ({ children }: Props) => {
   return (
     <Box width="300px" borderRadius={10} overflow="hidden">
