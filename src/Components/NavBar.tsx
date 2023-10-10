bd5cb68b5cb5181392fe94123be8bf20ce40f898
Gleb Kraitcer <kraitcer@gmail.com>
2023-10-10 15:32:37 +0400

    end

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,12 +1,21 @@
 import logo from "../assets/logo.webp";
+import { useNavigate } from "react-router-dom";
+
 import { HStack, Image } from "@chakra-ui/react";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
 
 const NavBar = () => {
+  const navigate = useNavigate();
+
   return (
     <HStack padding="10px">
-      <Image src={logo} boxSize="60px" />
+      <Image
+        src={logo}
+        boxSize="60px"
+        onClick={() => navigate("/")}
+        cursor={"pointer"}
+      />
       <SearchInput />
       <ColorModeSwitch />
     </HStack>
